6bf374ff76f2b20592aab381dd016c96caee75da
tugcebayri <tugceebayri@gmail.com>
2021-05-20 02:16:36 +0300

    create retrofit, and implement for login and register

---
+++ app/src/main/java/school/cactus/succulentshop/api/SucculentShopApi.kt
@@ -0,0 +1,18 @@
+package school.cactus.succulentshop.api
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import school.cactus.succulentshop.api.login.LoginRequest
+import school.cactus.succulentshop.api.login.LoginResponse
+import school.cactus.succulentshop.api.register.RegisterRequest
+import school.cactus.succulentshop.api.register.RegisterResponse
+
+interface SucculentShopApi {
+    @POST("/auth/local")
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+
+    @POST("/auth/local/register")
+    fun login(@Body registerRequest: RegisterRequest): Call<RegisterResponse>
+
+}
